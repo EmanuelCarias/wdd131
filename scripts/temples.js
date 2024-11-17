db5af45c9a9614a2bca14a03e0d7fb0432f62ab3
EmanuelCarias <jcarias484@gmail.com>
2024-11-16 19:39:33 -0500

    up

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+// Display the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Display the last modified date of the document
+document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
